80c2a55dcb638501e9c398aeffd46a907e957cd2
Sergi Cladera <sergicladera@paucasesnovescifp.cat>
2021-11-22 17:59:32 +0100

    Añadido contenido al button

--- Conflicts2/src/conflicts2/App.java
+++ Conflicts2/src/conflicts2/App.java
@@ -5,6 +5,8 @@
  */
 package conflicts2;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Alumne
@@ -147,7 +149,7 @@ public class App extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        JOptionPane.showMessageDialog(null, "Eggs are not supposed to be green.");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
